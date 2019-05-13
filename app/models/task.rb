17b8bf16a89affcff7be855fca53f5a48f937ee0
sho-kasama <sho-kasama@trunk.fm>
2019-05-13 20:43:36 +0900

    model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,11 @@
 class Task < ApplicationRecord
+
+    # 検証前の正規化
+    before_validation :set_nameless_name
+
+
+
     validates :name, presence: true, length: { maximum: 30 }
-
-
     # オリジナルな検証コードをかく
     validate :validate_name_not_including_comma
 
@@ -13,6 +17,11 @@ class Task < ApplicationRecord
       # &. ぼっち演算子を使って、nameがnilの場合には例外が発生するのを避けるために &.を利用して,nameがnilの時にはこの検証(errors.addをしない)ようにしている
       def validate_name_not_including_comma
         errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+      end
+
+      # コールバックの実装
+      def set_nameless_name
+        self.name = '名前なし' if name.blank?
       end
 
 
