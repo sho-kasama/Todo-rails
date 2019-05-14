352eea7b1a240759519f9f98d28bd900ccb0abdb
sho-kasama <sho-kasama@trunk.fm>
2019-05-14 23:59:28 +0900

    add: seed data

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
   
+  # タスク一覧を作成日時の新しい順に表示する
   def index
-    @tasks = Task.all
+    @tasks = currnet_user.tasks.order(created_at: :desc)
   end
 
   def show
