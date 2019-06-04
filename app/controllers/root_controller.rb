2bdb4ffe15668264d434931720ff8e3cf28e0e33
sho-kasama <sho-kasama@trunk.fm>
2019-06-04 17:03:03 +0900

    add :css

---
+++ app/controllers/root_controller.rb
@@ -0,0 +1,10 @@
+class RootController < ApplicationController
+
+  def about; end
+
+  def home; end
+
+  def rules; end
+
+  def privacy; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,32 @@
 Rails.application.routes.draw do
 
-  # HTTPメソッド getでログインのフォームを表示する
-  # GETメソッドで, '/login' というURLに対してリクエストがきたら
-  # SessionsControllerのnewというアクションを呼び出して欲しい
-  # また、'/login'というURLを,login_pathというヘルパーメソッドで生成できるようにする  
-  get '/login', to: 'sessions#new'
+  controller :root do 
+    get :about
+    get :rules
+    get :privacy
+    get :contact
+  end
 
-  # HTTPメソッド postでフォームから送られてきた情報を元にログインを行う
-  post '/login', to: 'sessions#create'
 
-  # HTTPメソッド DELETEでログアウトを行う
-  delete '/logout', to: 'sessions#destroy'
+  controller :tasks do
+    root 'tasks#index' 
+    resources :tasks do
+      post :confirm, action: :confirm_new, on: :new 
+      post :import, on: :collection
+    end
+  end
 
-  # route.rb でnamespace :adminのなかに定義を記述しているため
-  # URLに /admin, ヘルパーメソッド名にadmin_ がつくようになっている。
+
   namespace :admin do
     resources :users
   end
 
-  # タスクの一覧(index)が表示されるようにする
-  root to: 'tasks#index'
-  
-  # 確認画面は「CRUD」のためのアクションとしては見なされていないため、resoucesで一括で作られるルーティングとは別に定義してやる必要がある
-  # そこで保存前の(new)の確認をするという意味を込めて tasks/new/confirmというURLを confirm_newアクションに対応づけることにする
-  # routing に collectionを追加するとき 
-  # 今回ならPOSTリクエスト+/tasks/import　などの(idを伴なわない)パスを認識し、リクエストをtasksコントローラーのimportアクションに移動させる
 
-  resources :tasks do
-    post :confirm, action: :confirm_new, on: :new 
-    post :import, on: :collection
-  end
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
+
+  resources :contacts, only: %i(new create)
 
 end
